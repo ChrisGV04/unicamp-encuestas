df0d9ff377267aaa7d58621e3b00ef79160978a3
Christian Gil <christian@chrisgvdev.com>
2022-02-22 14:37:46 -0600

    update the baseURL in the client

--- client/src/hooks/axios.ts
+++ client/src/hooks/axios.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 export function useAxios() {
+  const PRODUCTION = import.meta.env.PROD;
+
   const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3030',
+    baseURL: PRODUCTION ? 'https://api.suiteescolar.com' : 'http://localhost:3030',
     withCredentials: true,
   });
 
